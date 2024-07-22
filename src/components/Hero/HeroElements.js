cfc6b700261873d65af60de102eb8683a5ce3392
Abhijeet Sharnagat <108009757+abhijeetsharnagat@users.noreply.github.com>
2024-07-22 11:59:57 +0530

    Update HeroElements.js

--- src/components/Hero/HeroElements.js
+++ src/components/Hero/HeroElements.js
@@ -1,127 +1,57 @@
-import styled, { keyframes } from "styled-components";
-import { Link as LinkScroll } from "react-scroll";
+import styled from "styled-components";
+import { Link as ScrollLink } from "react-scroll";
 
-export const HeroContainer = styled.div`
-  padding-bottom: 2rem;
-  padding-top: 4rem;
-  padding-right: 1rem;
-  padding-left: 1rem;
-  margin-right: auto;
-  margin-left: auto;
+export const MainContainer = styled.main`
   display: flex;
   flex-direction: column;
-
-  @media (min-width: 576px) {
-    max-width: 540px;
-  }
-  @media (min-width: 768px) {
-    max-width: 720px;
-  }
-  @media (min-width: 992px) {
-    max-width: 960px;
-  }
-  @media (min-width: 1200px) {
-    max-width: 1000px;
-  }
+  align-items: center;
+  justify-content: space-between;
+  height: 100vh;
+  padding: 20px;
+  background-color: #f0f0f0;
 `;
 
-export const HeroWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-
-  @media screen and (max-width: 992px) {
-    flex-direction: column;
-  }
-`;
-
-export const HeroLeft = styled.div`
+export const TopSection = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   justify-content: center;
-  align-items: flex-start;
-  text-align: left;
-  flex: 1;
-
-  h1 {
-    font-size: 2.8rem;
-    color: #f6f6f6;
-    opacity: 0.98;
-    font-weight: 400;
-  }
-
-  h5 {
-    font-size: 1.6rem;
-    color: rgb(119, 119, 121);
-    margin-bottom: 1rem;
-    font-weight: 400;
-  }
-
-  p {
-    font-size: 17px;
-    color: #f6f6f6;
-    opacity: 0.85;
-  }
-
-  @media screen and (max-width: 992px) {
-    text-align: center;
-    align-items: center;
-    margin-bottom: 2rem;
-
-    h5 {
-      min-height: 5rem;
-  }
+  height: 50%;
+  text-align: center;
 `;
 
-export const HeroRight = styled.div`
-  flex: 1;
+export const BottomSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
+  height: 50%;
+`;
+
+export const ProfileImage = styled.img`
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  margin-top: 20px;
+`;
+
+export const ScrollIndicator = styled(ScrollLink)`
   display: flex;
-`;
-
-export const Image = styled.img`
-  height: 300px;
-  width: auto;
-`;
-
-const ScrollAnimation = keyframes`
-  0%,
-  20%,
-  50%,
-  80%,
-  100% {
-    transform: translateY(0);
-  }
-  40% {
-    transform: translateY(-20px);
-  }
-  60% {
-    transform: translateY(-10px);
-  }
-`;
-
-export const ScrollDown = styled(LinkScroll)`
-  display: flex;
-  justify-content: flex-start;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
   cursor: pointer;
-  position: absolute;
-
-  animation: ${ScrollAnimation} 2s linear 0s infinite;
-  @media screen and (max-width: 992px) {
-    position: relative;
-    justify-content: center;
-    margin-top: 2rem;
-  }
 `;
 
-export const ScrollLink = styled.div`
+export const ScrollText = styled.div`
+  font-size: 18px;
+  color: #333;
   display: flex;
   align-items: center;
-  font-size: 1.3rem;
-  color: #f6f6f6;
+  gap: 10px;
 
   img {
-    height: 35px;
-    width: 35px;
-    margin-left: 6px;
+    width: 20px;
+    height: 20px;
   }
 `;
