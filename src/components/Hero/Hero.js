839ca06c9fbe237eaeeab37da3fe7e11e0d6a1b2
Abhijeet Sharnagat <108009757+abhijeetsharnagat@users.noreply.github.com>
2024-07-22 11:59:01 +0530

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,113 +1,63 @@
 import React, { useState } from "react";
 import Dropdown from "../Dropdown/Dropdown";
 import Header from "../Header/Header";
-import {
-  HeroContainer,
-  HeroWrapper,
-  HeroLeft,
-  HeroRight,
-  Image,
-  ScrollDown,
-  ScrollLink,
-} from "./HeroElements";
-import { TypeAnimation } from 'react-type-animation';
+import { TypeAnimation } from "react-type-animation";
 import ScrollAnimation from "react-animate-on-scroll";
+import {
+  MainContainer,
+  TopSection,
+  BottomSection,
+  ProfileImage,
+  ScrollIndicator,
+  ScrollText,
+} from "./NewHeroElements";
 
 function Hero() {
   const [isOpen, setIsOpen] = useState(false);
-  const [showSubtitle, setShowSubtitle] = useState(false);
   const [showScrollDown, setShowScrollDown] = useState(false);
 
   const toggle = () => {
     setIsOpen(!isOpen);
   };
+
   return (
-    <main>
+    <MainContainer>
       <Dropdown isOpen={isOpen} toggle={toggle} />
       <Header toggle={toggle} />
-      <HeroContainer>
-        <HeroWrapper>
-          <HeroLeft>
-            <ScrollAnimation animateIn="fadeIn" >
-              <TypeAnimation
-                cursor={false}
-                sequence={[
-                  'Hi, I\'m Meghraj.',
-                  () => setShowSubtitle(true)
-                ]}
-                speed={{ type: "keyStrokeDelayInMs", value: 150 }}
-                wrapper="h1"
-                repeat={0}
-              />
-              {showSubtitle &&
-                <TypeAnimation
-                  cursor={true}
-                  sequence={[
-                    500,
-                    'An Embedded Hardware Engineer.',
-                    1000,
-                    'A MIT Academy of Engineering graduate.',
-                    1000,
-                    'A problem solver.',
-                    1000,
-                    'An innovative thinker.',
-                    1000,
-                    'A....',
-                    1000,
-                    'A.... IoT enthusiast?',
-                    1000,
-                    "Ok...",
-                    1000,
-                    "Ok...  I'm passionate about embedded systems...",
-                    1000,
-                    "Uhh...",
-                    1000,
-                    "Uhh... you can scroll down to see my projects now...",
-                    300,
-                    () => setShowScrollDown(true),
-                    1000,
-                    "Seriously, my projects are really cool, go check them out!",
-                    1000,
-                    "You're uh...",
-                    1000,
-                    "You're uh... still here?",
-                    1000,
-                    "Ok, this has been fun, but I'm gonna restart the loop now...",
-                    1000,
-                    "See ya! :)",
-                    500,
-                  ]}
-                  speed={50}
-                  deletionSpeed={65}
-                  wrapper="h5"
-                  repeat={Infinity}
-                />
-              }
-            </ScrollAnimation>
-
-          </HeroLeft>
-          <HeroRight>
-            <ScrollAnimation animateIn="fadeIn">
-              <Image
-                src="/profile-pic.png"
-                alt="man-svgrepo"
-              />
-            </ScrollAnimation>
-          </HeroRight>
-        </HeroWrapper>
-        {showScrollDown &&<ScrollAnimation animateIn="flipInX" offset={0}>
-        <ScrollDown to="projects" id="scrollDown">
-          <ScrollLink>
-            Scroll down
-            <img
-              src="/scroll-down.svg"
-              alt="scroll-down"
-            />
-          </ScrollLink>
-        </ScrollDown>
-        </ScrollAnimation>}
-      </HeroContainer>
-    </main>
+      <TopSection>
+        <ScrollAnimation animateIn="fadeIn">
+          <TypeAnimation
+            cursor={true}
+            sequence={[
+              "Hi, I'm Meghraj.",
+              1000,
+              "An Embedded Hardware Engineer.",
+              1000,
+              () => setShowScrollDown(true),
+            ]}
+            speed={50}
+            deletionSpeed={65}
+            wrapper="h1"
+            repeat={Infinity}
+          />
+        </ScrollAnimation>
+      </TopSection>
+      <BottomSection>
+        <ScrollAnimation animateIn="fadeIn">
+          <ProfileImage src="/profile-pic.png" alt="man-svgrepo" />
+        </ScrollAnimation>
+        {showScrollDown && (
+          <ScrollAnimation animateIn="flipInX" offset={0}>
+            <ScrollIndicator to="projects" id="scrollDown">
+              <ScrollText>
+                Scroll down
+                <img src="/scroll-down.svg" alt="scroll-down" />
+              </ScrollText>
+            </ScrollIndicator>
+          </ScrollAnimation>
+        )}
+      </BottomSection>
+    </MainContainer>
   );
 }
 
